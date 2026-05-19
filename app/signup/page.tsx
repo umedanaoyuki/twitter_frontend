5dcb9fe3eda80f5711d1a24d46bffcfaa4885296
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-05-20 00:10:10 +0900

    feat: signupページのバックグラウンドのマークアップ

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { LandingPage } from "@/components/home/landing-page";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -16,37 +17,7 @@ export default function Signup() {
   return (
     <div>
       <main>
-        <div>
-          <div>
-            <div>
-              <span>すべての話題が、ここに。</span>
-            </div>
-            <div>
-              <span>今すぐ参加しましょう。</span>
-            </div>
-            <div>
-              <a href="/signup">
-                <div>
-                  <div>
-                    <span>アカウントを作成</span>
-                  </div>
-                </div>
-              </a>
-              <div>
-                アカウントを登録することにより、利用規約とプライバシーポリシー（Cookieの使用を含む）に同意したとみなされます。
-              </div>
-              <div>
-                <div>
-                  <span>アカウントをお持ちの場合</span>
-                </div>
-                <a href="/login">
-                  <span>ログイン</span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div>画像</div>
+        <LandingPage />
         <Dialog defaultOpen={true}>
           <form>
             <DialogContent className="sm:max-w-sm p-8">
