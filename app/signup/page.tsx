e5876cf493a01e38cfdbe615517c345e76979c28
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-05-20 21:07:33 +0900

    feat: cssの調整

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -16,19 +16,22 @@ import { InputWithCharCount } from "@/components/ui/input-with-char-count";
 import { useActionState } from "react";
 
 export default function Signup() {
-  const [state, formAction, pending] = useActionState<
-    RegisterState,
-    FormData
-  >(registerAction, null);
+  const [state, formAction, pending] = useActionState<RegisterState, FormData>(
+    registerAction,
+    null,
+  );
 
   return (
     <div>
       <main>
         <LandingPage />
         <Dialog defaultOpen={true}>
-          <DialogContent className="sm:max-w-sm p-8">
-            <form action={formAction}>
-              <DialogHeader className="mt-8">
+          <DialogContent className="flex h-[50%] flex-col p-8">
+            <form
+              action={formAction}
+              className="flex w-full flex-1 flex-col gap-6"
+            >
+              <DialogHeader>
                 <DialogTitle>アカウントを作成</DialogTitle>
               </DialogHeader>
               <FieldGroup>
@@ -65,7 +68,7 @@ export default function Signup() {
               {state?.success ? (
                 <p className="text-sm text-green-600">{state.message}</p>
               ) : null}
-              <DialogFooter>
+              <DialogFooter className="mt-auto w-full flex-col sm:flex-col">
                 <Button type="submit" className="w-full" disabled={pending}>
                   {pending ? "登録中..." : "登録する"}
                 </Button>
