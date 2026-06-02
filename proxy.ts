1cd61a55b71b2920007f4bc05c56bea1c31c856b
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-06-02 13:43:42 +0900

    feat: sessionの情報は .envからもってくるようにする

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,11 @@
 import { NextResponse, type NextRequest } from "next/server";
 
-import { SESSION_COOKIE_NAME } from "@/lib/session";
-
 export function proxy(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const isProtectedPath = path.startsWith("/home");
-  const session = request.cookies.get(SESSION_COOKIE_NAME)?.value;
+  const session = request.cookies.get(
+    process.env.SESSION_COOKIE_NAME as string,
+  )?.value;
 
   if (isProtectedPath && !session) {
     return NextResponse.redirect(new URL("/login", request.url));
