a54751d87653dc7c518f3fa77f634c79c1b16a58
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-07-03 01:22:31 +0900

    feat: アイコンのreact icon使用

--- components/home/tweet-card.tsx
+++ components/home/tweet-card.tsx
@@ -1,6 +1,9 @@
 import type { ReactNode } from "react";
 
 import type { Tweet } from "@/lib/types/tweet";
+import { FaComment } from "react-icons/fa";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { CiRepeat } from "react-icons/ci";
 
 type TweetCardProps = {
   tweet: Tweet;
@@ -105,37 +108,19 @@ function TweetCard({ tweet }: TweetCardProps) {
                 label={`返信 ${stats.replies}件`}
                 count={stats.replies}
               >
-                <svg
-                  viewBox="0 0 24 24"
-                  aria-hidden
-                  className="size-[18px] fill-current"
-                >
-                  <path d="M1.751 10c0-4.42 3.584-8 8.005-8h4.366c4.49 0 8.129 3.64 8.129 8.13 0 2.96-1.607 5.68-4.196 7.11l-8.054 4.46v-3.69h-.067c-4.49.1-8.183-3.51-8.183-8.01zm8.005-6.5c-3.584 0-6.5 2.916-6.5 6.5s2.916 6.5 6.5 6.5h.177l-.021.01v3.59l5.907-3.27c2.178-1.209 3.544-3.488 3.544-5.93 0-3.582-2.916-6.5-6.5-6.5-3.582 0-6.5 2.918-6.5 6.5z" />
-                </svg>
+                <FaComment />
               </TweetAction>
               <TweetAction
                 label={`リポスト ${stats.reposts}件`}
                 count={stats.reposts}
               >
-                <svg
-                  viewBox="0 0 24 24"
-                  aria-hidden
-                  className="size-[18px] fill-current"
-                >
-                  <path d="M4.5 3.88l4.432 4.14-1.364 1.46L5.5 7.55V16c0 1.1.896 2 2 2H13v2H7.5c-2.209 0-4-1.79-4-4V7.55L1.432 9.48.068 8.02 4.5 3.88zm15 12.12l-4.432-4.14 1.364-1.46L18.5 16.45V8c0-1.1-.896-2-2-2H11V4h5.5c2.209 0 4 1.79 4 4v8.45l3.068-1.93 1.364 1.46-4.432 4.14z" />
-                </svg>
+                <CiRepeat />
               </TweetAction>
               <TweetAction
                 label={`いいね ${stats.likes}件`}
                 count={stats.likes}
               >
-                <svg
-                  viewBox="0 0 24 24"
-                  aria-hidden
-                  className="size-[18px] fill-current"
-                >
-                  <path d="M16.697 3.48c-1.936 0-3.616 1.04-4.865 2.55C10.583 4.52 8.903 3.48 6.967 3.48c-2.776 0-5.027 2.25-5.027 5.027 0 5.75 9.188 11.727 9.188 11.727s9.188-5.977 9.188-11.727c0-2.777-2.251-5.027-5.027-5.027z" />
-                </svg>
+                <IoMdHeartEmpty />
               </TweetAction>
             </div>
           </div>
