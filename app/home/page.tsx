b621ef86561ff70a56332a0973604a201b152f5e
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-08-01 15:26:33 +0900

    refactor: 軽微な修正

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -12,8 +12,6 @@ export default async function Home() {
       error instanceof Error ? error.message : "投稿一覧の取得に失敗しました";
   }
 
-  console.log("timeline", timeline);
-
   return (
     <HomeTimeline
       timeline={
